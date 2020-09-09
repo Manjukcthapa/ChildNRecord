89984c05ae7711dbb77022505eba5c1ecd3a1f4e
Manju KC <kcmanjuthapa@gmail.com>
2020-09-09 18:22:45 -0400

    added inputs and render the signup component in browser

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Signup from "./Register/Signup"
 
 function App() {
   return (
     <div className="App">
-     <h1>hello World</h1>
+     <Signup/>
     </div>
   );
 }
--- my-app/src/Register/Signup.js
+++ my-app/src/Register/Signup.js
@@ -1,18 +1,39 @@
 import React from "react";
 
 class Signup extends React.Component {
-  render() {
-    <div>
-      <form>
-        <input
-          type="text"
-          id="Username"
-          placeholder="Enter Your Username "
-          name="Username"
-        />
-      </form>
-    </div>;
-  }
+
+  render(){
+      return(
+          <div>
+              <form>
+                  <lable>UserName</lable>
+                  <input
+                  type="text"
+                  id="Username"
+                  placeholder="Enter Your Username "
+                  name="Username"
+                  />
+                   <lable>Password</lable>
+                  <input
+                  type="text"
+                  id="Password"
+                  placeholder="Enter Your Password"
+                  name="Password"
+                  />
+                  <lable>Re-Password</lable>
+                  <input
+                  type="text"
+                  id="Re-Password"
+                  placeholder="Enter Your Re-Password"
+                  name="Re-Password"
+                  />
+              </form>
+          </div>
+      )
+    
+}
+
 }
 
 export default Signup;
+
