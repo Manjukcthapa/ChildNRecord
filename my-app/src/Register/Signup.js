8fc95c7da033f0e1a9c92a656c3b8fec764abcc5
Manju KC <kcmanjuthapa@gmail.com>
2020-09-09 18:10:13 -0400

    created signup component

---
+++ my-app/src/Register/Signup.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Signup extends React.Component {
+  render() {
+    <div>
+      <form>
+        <input
+          type="text"
+          id="Username"
+          placeholder="Enter Your Username "
+          name="Username"
+        />
+      </form>
+    </div>;
+  }
+}
+
+export default Signup;
