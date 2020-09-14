d4860c031e116453b4ab09be41dc07e846ec7d3a
Manju KC <kcmanjuthapa@gmail.com>
2020-09-14 18:49:09 -0400

    added handlechange function in signup component

--- my-app/src/Register/Signup.js
+++ my-app/src/Register/Signup.js
@@ -1,48 +1,58 @@
 import React from "react";
 
 class Signup extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-        username: '',
-        password: '',
-        repassword: ''
-        }
-        }
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      repassword: "",
+    };
+  }
 
+  handleChange = e => {
+    let target = e.target;
+    let value = target.value;
+    let name = target.name
+    this.setState({ [name]: value })
+    console.log(e.target.value)
+  }
 
-  render(){
-      return(
-          <div>
-              <form>
-                  <lable>UserName</lable>
-                  <input
-                  type="text"
-                  id="Username"
-                  placeholder="Enter Your Username "
-                  name="Username"
-                  />
-                   <lable>Password</lable>
-                  <input
-                  type="text"
-                  id="Password"
-                  placeholder="Enter Your Password"
-                  name="Password"
-                  />
-                  <lable>Re-Password</lable>
-                  <input
-                  type="text"
-                  id="Re-Password"
-                  placeholder="Enter Your Re-Password"
-                  name="Re-Password"
-                  />
-              </form>
-          </div>
-      )
-    
-}
-
+  render() {
+    return (
+      <div>
+        <form>
+          <lable>UserName</lable>
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter Your Username "
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <lable>Password</lable>
+          <input
+            type="text"
+            id="password"
+            placeholder="Enter Your Password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <lable>Re-Password</lable>
+          <input
+            type="text"
+            id="repassword"
+            placeholder="Enter Your Re-Password"
+            name="repassword"
+            value={this.state.repassword}
+            onChange={this.handleChange}
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Signup;
-
