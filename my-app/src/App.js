10a514aa24a1371e0a921d55b62857b86f027ac2
Manju KC <kcmanjuthapa@gmail.com>
2020-09-18 12:18:29 -0400

    created header component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import SignUp from "./Register/Signup"
-import LogIn from "./Register/LogIn"
+import SignUp from "./Component/Register/Signup"
+import LogIn from "./Component/Register/LogIn"
 import { Route, Switch } from 'react-router-dom'
+import Header from "./Component/Header"
 
 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
+      
       <Route exact path="/signup" component={SignUp} />
       <Route  path="/LogIn" component={LogIn} />
      </Switch>
